c829b201fd05b4cc2f78ef408befad2a598d7a60
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-07-01 16:19:39 -0300

    Exercicio 04

---
+++ Aulas txt/Exercicios/Exercicio 04/Exercicio.java
@@ -0,0 +1,39 @@
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Date;
+import java.util.Locale;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emanuel
+ */
+public class Exercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        Date data = new Date();
+        
+        System.out.print("A hora do sistema é ");
+        
+        System.out.println(data.toString());
+        
+        Locale loc = Locale.getDefault();
+
+        String idioma = loc.getDisplayLanguage();
+
+        System.out.println("Seu sistema está em " + idioma);
+        
+        Dimension tamanhoTela = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println(tamanhoTela.getSize());
+    }
+}
